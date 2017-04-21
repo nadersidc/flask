9720bf3b1c124dc1ec42b6437f61b96ac8484a41
nadersidc <nadersidc@outlook.com>
2017-04-21 22:43:25 +0800

    add test.py

---
+++ test.py
@@ -0,0 +1,26 @@
+# encoding: utf-8
+
+
+"""
+@version: ??
+@author: phpergao
+@license: Apache Licence 
+@contact: endoffight@gmail.com
+@site: http://www.nadersidc.com
+@software: PyCharm
+@file: test.py
+@time: 2017/4/21 下午10:40
+"""
+
+
+def func():
+    pass
+
+
+class Main():
+    def __init__(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
